f5f051d568645aae5108bd2266c7bb595f6d709e
Colin <zzcpeter27@qq.com>
2018-12-05 00:06:55 +0800

    CollapsingToolbarLayout

--- app/src/main/java/com/example/colin/myapplication/PYQActivity.java
+++ app/src/main/java/com/example/colin/myapplication/PYQActivity.java
@@ -1,11 +1,18 @@
 package com.example.colin.myapplication;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PYQActivity extends AppCompatActivity {
 
@@ -16,13 +23,48 @@ public class PYQActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //返回按钮
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.back);
+        }
+
+        TextView newTitel = findViewById(R.id.newTitle);
+        ImageView userIcon = findViewById(R.id.userIcon);
+        //TODO toolbar 背景图片取不到
+        //TODO 头像在 toolbar下方
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_pyq,menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item_camera:
+            //TODO 发朋友圈
+                Toast.makeText(PYQActivity.this,"camera touched",Toast.LENGTH_SHORT).show();
+                break;
+            case android.R.id.home:
+                Toast.makeText(PYQActivity.this,"home touched",Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+        return true;
     }
 }
